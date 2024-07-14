9fd7621eff1e5c4db380e38d6eb3264ecb60ecf3
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2024-07-14 20:46:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function firstWord(s) {
-  // your code here
-}
-
+function firstWord(str) {
+    // Check if the string is empty or does not contain any space
+    if (str === "" || !str.includes(" ")) {
+        return str;
+    }
+    
+    // Find the index of the first space
+    const firstSpaceIndex = str.indexOf(" ");
+    
+    // Return the substring from the start to the first space
+    return str.substring(0, firstSpaceIndex);
+}   
 // Do not change the code below
 
 const s = prompt("Enter String:");
